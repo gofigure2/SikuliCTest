6799a46c8d9c805084f4994b47ce69f954c6a446
sikuli <sikuli@sikuli-desktop.(none)>
2011-06-23 14:07:09 -0400

    Change example test to launch firefox, go to midasjournal website
    (and search for sikuli's paper)

--- test.sikuli/test.py
+++ test.sikuli/test.py
@@ -1,9 +1,13 @@
 def setUp(self):
-  print "setUp";
+	print "setUp";
 
-def testThunderbird(self):
-  print "Click on thunderbird";
-  click("1299697212820.png")
+def testFirefox(self):
+	print "Click on firefox";
+	click("1308851838899.png")
+	doubleClick("1308852041788.png")
+	type("http://midasjournal.org"+Key.ENTER)
+	click("1308852315244.png")
+	type("sikuli"+Key.ENTER)
 
 def tearDown(self):
-  print "tearDown";
+	print "tearDown";
