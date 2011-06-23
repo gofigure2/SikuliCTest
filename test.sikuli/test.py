5c92352b7e3030375b7357d260db7c0d42b33fac
Evan Schwab <eschwab8@gmail.com>
2011-06-23 14:51:11 -0400

    add some wait to make sure previous actions get done

--- test.sikuli/test.py
+++ test.sikuli/test.py
@@ -4,10 +4,13 @@ def setUp(self):
 def testFirefox(self):
 	print "Click on firefox";
 	click("1308851838899.png")
+	wait(10)
 	doubleClick("1308852041788.png")
 	type("http://midasjournal.org"+Key.ENTER)
+	wait(10)
 	click("1308852315244.png")
 	type("sikuli"+Key.ENTER)
 
 def tearDown(self):
 	print "tearDown";
+
